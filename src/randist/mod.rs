7210ae0eab24cb1226a0b27865e9aa2e941d64a6
Rickard Kjellin <rickard.kjellin@gmail.com>
2014-11-29 22:30:39 +0100

    initial functional commit

---
+++ src/randist/mod.rs
@@ -0,0 +1,12 @@
+#[link(name = "lgsl")]
+extern {
+    pub fn gsl_ran_ugaussian_pdf(x: f64) -> f64;
+    //double gsl_ran_ugaussian_pdf (const double x);
+}
+
+#[test]
+fn test_gsl_ran_ugaussian_pdf() {
+    let res = unsafe {
+        gsl_ran_ugaussian_pdf(1f64)
+    };
+}
