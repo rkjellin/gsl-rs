52073b604d491416c6944964385382c798123620
Rickard Kjellin <rickard.kjellin@gmail.com>
2014-11-30 22:40:30 +0100

    expose safe wrapper

--- src/randist/mod.rs
+++ src/randist/mod.rs
@@ -1,13 +1,21 @@
-#[link(name = "gsl")]
-#[link(name = "gslcblas")]
-extern {
-    pub fn gsl_ran_ugaussian_pdf(x: f64) -> f64;
-    //double gsl_ran_ugaussian_pdf (const double x);
+mod bindings {
+    #[link(name = "gsl")]
+    #[link(name = "gslcblas")]
+    extern {
+        pub fn gsl_ran_ugaussian_pdf(x: f64) -> f64;
+    }
+}
+
+pub mod ugaussian {
+    use super::bindings;
+    pub fn pdf(x: f64) -> f64 {
+        unsafe { bindings::gsl_ran_ugaussian_pdf(x) }
+    }
 }
 
 #[test]
-fn test_gsl_ran_ugaussian_pdf() {
-    let res = unsafe {
-        gsl_ran_ugaussian_pdf(1f64)
-    };
+fn calcpfd() {
+    use randist::ugaussian;
+    ugaussian::pdf(1.0f64);
 }
+
